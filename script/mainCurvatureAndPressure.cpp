4e9f40801b06168018f726c983e7ffc37cbef3f6
Gustavo ANJOS <gustavo.rabello@gmail.com>
2011-10-04 19:21:32 +0200

    Add more precision at saveBubbleInfo and add more test cases to mainCurvatureAndPressure.

--- script/mainCurvatureAndPressure.cpp
+++ script/mainCurvatureAndPressure.cpp
@@ -52,7 +52,7 @@ int main(int argc, char **argv)
 
  /* meshes */
  vector<const char*> mesh;
- mesh.resize(25);
+ mesh.resize(34);
  mesh[0]  = "../../db/gmsh/3d/curvature/0.25.msh";
  mesh[1]  = "../../db/gmsh/3d/curvature/0.24.msh";
  mesh[2]  = "../../db/gmsh/3d/curvature/0.23.msh";
@@ -69,15 +69,24 @@ int main(int argc, char **argv)
  mesh[13] = "../../db/gmsh/3d/curvature/0.12.msh";
  mesh[14] = "../../db/gmsh/3d/curvature/0.11.msh";
  mesh[15] = "../../db/gmsh/3d/curvature/0.10.msh";
- mesh[16] = "../../db/gmsh/3d/curvature/0.09.msh";
- mesh[17] = "../../db/gmsh/3d/curvature/0.08.msh";
- mesh[18] = "../../db/gmsh/3d/curvature/0.07.msh";
- mesh[19] = "../../db/gmsh/3d/curvature/0.06.msh";
- mesh[20] = "../../db/gmsh/3d/curvature/0.05.msh";
- mesh[21] = "../../db/gmsh/3d/curvature/0.04.msh";
- mesh[22] = "../../db/gmsh/3d/curvature/0.03.msh";
- mesh[23] = "../../db/gmsh/3d/curvature/0.02.msh";
- mesh[24] = "../../db/gmsh/3d/curvature/0.01.msh";
+ mesh[16] = "../../db/gmsh/3d/curvature/0.095.msh";
+ mesh[17] = "../../db/gmsh/3d/curvature/0.09.msh";
+ mesh[18] = "../../db/gmsh/3d/curvature/0.085.msh";
+ mesh[19] = "../../db/gmsh/3d/curvature/0.08.msh";
+ mesh[20] = "../../db/gmsh/3d/curvature/0.075.msh";
+ mesh[21] = "../../db/gmsh/3d/curvature/0.07.msh";
+ mesh[22] = "../../db/gmsh/3d/curvature/0.065.msh";
+ mesh[23] = "../../db/gmsh/3d/curvature/0.06.msh";
+ mesh[24] = "../../db/gmsh/3d/curvature/0.055.msh";
+ mesh[25] = "../../db/gmsh/3d/curvature/0.05.msh";
+ mesh[26] = "../../db/gmsh/3d/curvature/0.045.msh";
+ mesh[27] = "../../db/gmsh/3d/curvature/0.04.msh";
+ mesh[28] = "../../db/gmsh/3d/curvature/0.035.msh";
+ mesh[29] = "../../db/gmsh/3d/curvature/0.03.msh";
+ mesh[30] = "../../db/gmsh/3d/curvature/0.025.msh";
+ mesh[31] = "../../db/gmsh/3d/curvature/0.02.msh";
+ mesh[32] = "../../db/gmsh/3d/curvature/0.015.msh";
+ mesh[33] = "../../db/gmsh/3d/curvature/0.01.msh";
 
  for( int i=0;i<(int) mesh.size();i++ )
  {

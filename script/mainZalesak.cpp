63a1a9fcdd28eb2c741e589c9f58a5429fb25af4
Gustavo ANJOS <gustavo.rabello@gmail.com>
2012-10-21 19:54:57 -0400

    Adding 3D Zalesak test case.

--- script/mainZalesak.cpp
+++ script/mainZalesak.cpp
@@ -12,6 +12,7 @@
 #include "TElement.h"
 #include "GMRes.h"
 #include "InOut.h"
+#include "Helmholtz3D.h"
 #include "PetscSolver.h"
 #include "petscksp.h"
 #include "colors.h"
@@ -22,13 +23,13 @@ int main(int argc, char **argv)
 {
  PetscInitialize(&argc,&argv,PETSC_NULL,PETSC_NULL);
  
- // static bubble test (Fabricio's thesis (2005))
+ int iter = 1;
  real Re = 100;
  real We = 0.2;
  real c1 = 0.0;  // lagrangian
  real c2 = 0.5;  // smooth vel
  real c3 = 3.0;  // smooth coord (fujiwara)
- real d1 = 1.0;  // surface tangent velocity u_n=u-u_t 
+ real d1 = 0.0;  // surface tangent velocity u_n=u-u_t 
  real d2 = 0.1;  // surface smooth cord (fujiwara)
  real alpha = 1;
 
@@ -38,7 +39,7 @@ int main(int argc, char **argv)
  real rho_in = 1.0;
  real rho_out = 0.01;
 
- real cfl = 1.0;
+ real cfl = 0.8;
 
  string meshFile = "sphere.msh";
 
@@ -71,7 +72,7 @@ int main(int argc, char **argv)
  m1.setSurfaceConfig();
  m1.setInitSurfaceVolume();
  m1.setInitSurfaceArea();
- m1.setWallBC();
+ m1.setVortexFieldBC();
 
  s1(m1);
 
@@ -93,52 +94,136 @@ int main(int argc, char **argv)
  s1.setSolverVelocity(solverV);
  s1.setSolverConcentration(solverC);
 
+ // Point's distribution
+ Helmholtz3D h1(m1);
+ h1.setBC();
+ h1.initRisingBubble();
+ h1.assemble();
+ h1.setk(0.2);
+ h1.matMountC();
+ h1.setUnCoupledCBC(); 
+ h1.setCRHS();
+ h1.unCoupledC();
+ //h1.saveVTK(vtkFolder,"edge");
+ h1.setModel3DEdgeSize();
+
  InOut save(m1,s1); // cria objeto de gravacao
  save.saveVTK(vtkFolder,"geometry");
  save.saveVTKSurface(vtkFolder,"geometry");
  save.saveMeshInfo(datFolder);
  save.saveInfo(datFolder,"info",mesh);
 
- int nIter = 1000;
- for( int i=1;i<nIter;i++ )
+ int nIter = 3000;
+ int nReMesh = 1;
+ for( int i=1;i<=nIter;i++ )
  {
-  cout << color(none,magenta,black);
-  cout << "____________________________________ Iteration: " 
-       << i << endl << endl;
-  cout << resetColor();
+  for( int j=0;j<nReMesh;j++ )
+  {
 
-  s1.setDtALETwoPhase();
+   cout << color(none,magenta,black);
+   cout << "____________________________________ Iteration: " 
+	    << iter << endl << endl;
+   cout << resetColor();
 
-  InOut save(m1,s1); // cria objeto de gravacao
-  save.printSimulationReport();
+   s1.setDtALETwoPhase();
 
-  //s1.stepLagrangian();
-  //s1.stepALE();
-  s1.stepALEVel();
-  s1.movePoints();
-  s1.assemble();
-  s1.matMount();
-  s1.setUnCoupledBC();
-  s1.setRHS();
-  //s1.setInterface();
-  s1.setInterfaceGeo();
-  s1.unCoupled();
+   InOut save(m1,s1); // cria objeto de gravacao
+   save.printSimulationReport();
 
-  save.saveMSH(mshFolder,"newMesh",i);
-  save.saveVTK(vtkFolder,"sim",i);
-  save.saveVTKSurface(vtkFolder,"sim",i);
-  save.saveSol(binFolder,"sim",i);
-  save.saveBubbleInfo(datFolder);
-  save.saveParasiticCurrent(datFolder);
-  save.chordalPressure(datFolder,"chordalPressure",i);
-  //save.saveMeshInfo(datFolder);
+   //s1.stepLagrangian();
+   //s1.stepALE();
+   s1.stepALEVel();
+   s1.movePoints();
+   s1.assemble();
+   s1.matMount();
+   s1.setUnCoupledBC();
+   s1.setRHS();
+   //s1.setGravity("-Z");
+   //s1.setInterface();
+   //s1.setInterfaceGeo();
+   s1.unCoupled();
 
-  s1.saveOldData();
+   save.saveMSH(mshFolder,"newMesh",iter);
+   save.saveVTK(vtkFolder,"sim",iter);
+   save.saveVTKSurface(vtkFolder,"sim",iter);
+   save.saveSol(binFolder,"sim",iter);
+   save.saveBubbleInfo(datFolder);
+   //save.crossSectionalVoidFraction(datFolder,"voidFraction",iter);
 
-  cout << color(none,magenta,black);
-  cout << "________________________________________ END of " 
-       << i << endl << endl;;
-  cout << resetColor();
+   s1.saveOldData();
+
+   cout << color(none,magenta,black);
+   cout << "________________________________________ END of " 
+	    << iter << endl << endl;;
+   cout << resetColor();
+
+   iter++;
+  }
+  Helmholtz3D h2(m1,h1);
+  h2.setBC();
+  h2.initRisingBubble();
+  h2.assemble();
+  h2.setk(0.2);
+  h2.matMountC();
+  h2.setUnCoupledCBC(); 
+  h2.setCRHS();
+  h2.unCoupledC();
+  h2.saveVTK(vtkFolder,"edge",iter-1);
+  h2.saveChordalEdge(datFolder,"edge",iter-1);
+  h2.setModel3DEdgeSize();
+
+  Model3D mOld = m1; 
+
+  /* *********** MESH TREATMENT ************* */
+  // set normal and kappa values
+  m1.setNormalAndKappa();
+  m1.initMeshParameters();
+
+  // 3D operations
+  //m1.insert3dMeshPointsByDiffusion();
+  m1.remove3dMeshPointsByDiffusion();
+  //m1.removePointByVolume();
+  //m1.removePointsByInterfaceDistance();
+  //m1.remove3dMeshPointsByDistance();
+  m1.remove3dMeshPointsByHeight();
+  m1.delete3DPoints();
+
+  // surface operations
+  m1.smoothPointsByCurvature();
+
+  m1.insertPointsByLength();
+  //m1.insertPointsByCurvature();
+  //m1.removePointsByCurvature();
+  //m1.insertPointsByInterfaceDistance();
+  m1.contractEdgeByLength();
+  //m1.removePointsByLength();
+  m1.flipTriangleEdge();
+
+  m1.removePointByNeighbourCheck();
+  //m1.checkAngleBetweenPlanes();
+  /* **************************************** */
+
+  //m1.mesh2Dto3DOriginal();
+  m1.mesh3DPoints();
+#if NUMGLEU == 5
+ m1.setMiniElement();
+#else
+ m1.setQuadElement();
+#endif
+  m1.setOFace();
+  m1.setSurfaceConfig();
+  m1.setGenericBC();
+
+  Simulator3D s2(m1,s1);
+  s2.applyLinearInterpolation(mOld);
+  s1 = s2;
+  s1.setSolverPressure(solverP);
+  s1.setSolverVelocity(solverV);
+  s1.setSolverConcentration(solverC);
+
+  InOut saveEnd(m1,s1); // cria objeto de gravacao
+  saveEnd.printMeshReport();
+  saveEnd.saveMeshInfo(datFolder);
  }
 
  PetscFinalize();

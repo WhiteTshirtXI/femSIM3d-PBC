689bde8406e59938320ec8ec73daf8fb430a1332
Gustavo Charles Peixoto de Oliveira <tavolesliv@gmail.com>
2014-04-16 13:08:32 -0300

    Adding scripts for mainsPBC.

---
+++ script/mainChannelPBC.cpp
@@ -0,0 +1,284 @@
+/* File: mainChannelPBC.cpp                                            
+ * Created on February 17th, 2014                                      
+ * Author: Gustavo Charles P. de Oliveira                              
+ * e-mail: tavolesliv@gmail.com
+ * Maintainance: Gustavo Rabello dos Anjos                            
+ * E-mail: gustavo.rabello@gmail.com                                   
+ *
+ * Description: version adapted from mainStep.cpp to include periodic 
+ * boundary conditions. Suitable to run single-phase simulations for 
+ * longitudinal channels. PBC is implemented to work with meshes whose
+ * x-axis is the streamwise direction of spatial periodicity.
+ *
+ * \remark: PBC working for velocity and pressure. 
+ *         To be implemented to further scalar fields, such as
+ *         temperature.  
+ * 
+ *
+ *  ============
+ *   Guidelines
+ *  ============
+ * 
+ *
+ * # PRE-PROCESSING
+ *
+ * - Choose mesh file extension: vtk or msh;
+ * - Introduce path to file;
+ * - Call method to check spatial periodicity of the mesh;
+ * 
+ * # PBC
+ *
+ * - Impose pressure gradient based on input parameters;
+ *
+ *
+ * # ITERATIVE STEP
+ *
+ * - Allow gravity effects or not;
+ * - Allow pressure gradient or not;
+ *
+ */
+
+// C++ Header Files
+#include <cmath>
+
+// Code Header Files
+#include "Model3D.h"
+#include "CGSolver.h"
+#include "PCGSolver.h"
+#include "GMRes.h"
+#include "Simulator3D.h"
+#include "TElement.h"
+#include "InOut.h"
+#include "PetscSolver.h"
+#include "petscksp.h"
+#include "Periodic3D.h"
+
+// Number of Phases (Do not change!)
+#define NUMPHASES 1
+
+// Main function
+int main(int argc, char **argv)
+{
+ /* PRE-PROCESSING SECTION */
+
+ PetscInitialize(&argc,&argv,PETSC_NULL,PETSC_NULL);
+
+
+ int iter = 1;
+ //double alpha = 1;
+ double cfl = 0.1;
+
+ double Re = 10000.0;
+ double Sc = 200;
+ double Fr = 100;
+ double mu_l = 1.0;
+ double rho_l = 1.0;
+ 
+ //** Solver and Pre-Conditioner Choice - pressure, velocity, scalar
+ //Solver *solverP = new PCGSolver();
+ //Solver *solverP = new PetscSolver(KSPGMRES,PCJACOBI);
+ Solver *solverP = new PetscSolver(KSPCG,PCJACOBI);
+ //Solver *solverP = new PetscSolver(KSPPREONLY,PCLU);
+ //Solver *solverP = new PetscSolver(KSPLSQR,PCILU);
+ Solver *solverV = new PetscSolver(KSPCG,PCICC);
+ //Solver *solverV = new PCGSolver();
+ Solver *solverC = new PCGSolver();
+
+ //** Data Saving Directories
+ //const char *binFolder  = "./bin/";
+ //const char *datFolder  = "./dat/";
+ //const char *datFolder  = "./sol/";
+ //const char *txtFolder  = "./txt/";
+ //const char *vtkFolder = "/home/gcpoliveira/post-processing/vtk/3d/poiseuille-pbc/";
+ //const char *vtkFolder = "/home/gcpoliveira/post-processing/vtk/3d/midwall-pbc/";
+ //const char *vtkFolder = "/home/gcpoliveira/post-processing/vtk/3d/taylor-vortex/";
+ const char *vtkFolder = "/home/gcpoliveira/post-processing/vtk/3d/taylor-green-vortex/";
+
+ //** Model Constructor
+ Model3D m1;
+
+ //** Mesh Loading: Selection of File and Path
+ 
+ string selectionExtension = "msh";
+
+ const char *mesh = NULL;
+ if ( selectionExtension == "msh")
+ {
+  	//*** File
+ 	string meshFile = "cuboid-3d.msh";
+ 	//string meshFile = "thesis-jet.msh";
+ 	//string meshFile = "cuboid-3d-w0.1.msh";
+ 	//string meshFile = "cylinder-3d.msh";
+
+ 	string meshDir = (string) getenv("MESH3D_DIR");
+ 	meshDir += "/" + meshFile;
+ 	const char *aux = meshDir.c_str();
+	mesh = aux;
+
+	//** Model Objects Call
+ 	m1.readMSH(mesh);
+ 	m1.setInterfaceBC();
+ 	m1.setTriEdge();
+ 	m1.mesh2Dto3D();
+ 	m1.setMapping();
+ 
+	#if NUMGLEU == 5
+ 		m1.setMiniElement();
+	#else
+ 		m1.setQuadElement();
+	#endif
+ 	
+ 	m1.setVertNeighbour();
+ 	m1.setInOutVert();
+	m1.setGenericBC();
+ 	//m1.setOnePointPressureBC();
+ }
+ else if ( selectionExtension == "vtk" )
+ {
+	//*** File
+ 	string meshFile = "cube-h0.05.vtk";
+
+ 	string meshDir = (string) getenv("MESH3D_DIR");
+ 	meshDir += "/" + meshFile;
+ 	const char *aux = meshDir.c_str();
+	mesh = aux;
+	
+	//** Model Objects Call
+	m1.readVTK(mesh);
+	#if NUMGLEU == 5
+		m1.setMiniElement();
+	#else
+		m1.setQuadElement();
+	#endif
+	m1.setMapping();
+	m1.setInOutVert();
+	m1.setCubeVortexBC();
+	m1.setOnePointPressureBC();
+ }
+ else
+ {
+	cerr << "Error Check mesh file: vtk/msh." << endl; 
+	exit(1);
+ }
+
+ //* Periodic Objets Call
+ Periodic3D pbc(m1);
+ //pbc.MountPeriodicVectorsNew(m1);
+ pbc.MountPeriodicVectors(m1);
+
+ //** Simulator Objects Call
+ Simulator3D sp(pbc,m1);
+ 
+ //*** Further Setting
+ //**** Physics
+ sp.setRe(Re);
+ sp.setSc(Sc);
+ sp.setFr(Fr);
+ sp.setMu(mu_l);
+ sp.setRho(rho_l);
+
+ //**** Numerics
+ sp.setCfl(cfl);
+ sp.setDtEulerian();
+
+ //**** Solver
+ sp.setSolverPressure(solverP);
+ sp.setSolverVelocity(solverV);
+ sp.setSolverConcentration(solverC);
+
+ //*** Initial Conditions
+ //sp.init(); // default
+ sp.initTaylorVortex(); // Taylor vortex
+ //sp.initTaylorGreenVortex();
+
+ //*** Matrix Mounting and b.c. Setting - Velocity, Pressure
+ sp.assemble();
+ sp.matMount();
+ //sp.matMountC();
+
+ //*** Starting Flow: Pressure Gradient Setting
+ sp.setBetaPressureLiquid();
+
+
+ /* PROCESSING / POST-PROCESSING SECTION */
+
+ if( (*(argv+1)) == NULL )
+ {
+  cout << endl;
+  cout << "--------------> STARTING FROM 0" << endl;
+  cout << endl;
+ }
+ else if( strcmp( *(argv+1),"restart") == 0 )
+ {
+  cout << endl;
+  cout << "--------------> RE-STARTING..." << endl;
+  cout << endl;
+
+  string file = (string) "sim-" + *(argv+2);
+  iter = sp.loadSolution("./","sim",atoi(*(argv+2)));
+ }
+
+ //** Save Objects Call
+ InOut save(m1,sp); // cria objeto de gravacao
+ 
+ //*** Mesh Information
+ save.saveVTK(vtkFolder,"geometry");
+ save.saveInfo("./","info",mesh);
+
+ //*** Output (Initial Condition)
+ save.saveVTK(vtkFolder,"initial",0);
+
+ //*** Iterative Process (Temporal Loop)
+ int nIter = 50;
+ int nRe = 1;
+ for( int i=0;i<nIter;i++ )
+ {
+  for( int j=0;j<nRe;j++ )
+  {
+   cout << "____________________________________ Iteration: " 
+	    << iter << endl;
+
+   //**** Advective Term
+   //sp.stepSLPBCFix();
+   //sp.stepNoConvection();
+   sp.stepSL();
+   
+   //**** B.C. update
+   //sp.setUnCoupledPBC(); 
+   sp.setUnCoupledBC(); 
+
+   //**** Physical Effects
+   //sp.setGravity("+X");
+   //sp.setBetaFlowLiq("+X");
+
+   //**** r.h.s Vector
+   sp.setRHS_PBC();
+
+   //**** Periodic Copy
+   sp.setCopyDirectionPBC("RL");
+   
+   //**** Matricial System Solution
+   sp.unCoupledPBC();
+   //sp.unCoupled();
+   
+   //**** Solution Saving
+   save.saveVTK(vtkFolder,"sim",iter);
+   //save.saveVTU(vtkFolder,"sim",iter);
+   //save.saveSol(binFolder,"sim",iter);
+
+   //**** Updating Quantities
+   sp.saveOldData();
+
+   //**** Updating Time
+   sp.timeStep();
+
+   cout << "________________________________________ END of "
+	    << iter << endl;
+
+   iter++;
+  }
+ }
+
+ PetscFinalize();
+ return 0;
+}
---
+++ script/mainMovingFramePBC.cpp
@@ -0,0 +1,442 @@
+/* File: mainMovingFramePBC.cpp                                            
+ * Created on February 17th, 2014                                      
+ * Author: Gustavo Charles P. de Oliveira                              
+ * e-mail: tavolesliv@gmail.com
+ * Maintainance: Gustavo Rabello dos Anjos                            
+ * E-mail: gustavo.rabello@gmail.com                                   
+ *
+ * Description: version adapted from mainMovingFrame.cpp to include periodic 
+ * boundary conditions. Suitable to run two-phase simulations for 
+ * longitudinal channels. PBC is implemented to work with meshes whose
+ * x-axis is the streamwise direction of spatial periodicity.
+ *
+ * \remark: PBC working for velocity and pressure. 
+ *         To be implemented to further scalar fields, such as
+ *         temperature.  
+ * 
+ *
+ *  ============
+ *   Guidelines
+ *  ============
+ * 
+ *
+ * # PRE-PROCESSING
+ *
+ * - Choose mesh file extension: vtk or msh;
+ * - Introduce path to file;
+ * - Call method to check spatial periodicity of the mesh;
+ * 
+ * # PBC
+ *
+ * - Impose pressure gradient based on input parameters;
+ *
+ *
+ * # ITERATIVE STEP
+ *
+ * - Allow gravity effects or not;
+ * - Allow pressure gradient or not;
+ *
+ */
+
+// C++ Header Files
+#include <cmath>
+
+// Code Header Files
+#include "Model3D.h"
+#include "Simulator3D.h"
+#include "CGSolver.h"
+#include "PCGSolver.h"
+#include "TElement.h"
+#include "GMRes.h"
+#include "InOut.h"
+#include "Helmholtz3D.h"
+#include "PetscSolver.h"
+#include "petscksp.h"
+#include "colors.h"
+#include "Periodic3D.h"
+
+// Number of phases (Do not change!)
+#define NUMPHASES 2
+
+// Main function
+int main(int argc, char **argv)
+{
+ /* PRE-PROCESSING SECTION */
+
+ PetscInitialize(&argc,&argv,PETSC_NULL,PETSC_NULL);
+ //PetscInitializeNoArguments();
+
+ //** Numerical Parameters
+ // bogdan's thesis 2010 (Bhaga and Weber, JFM 1980)
+ int iter = 1;
+ double vel = 0;
+ double c1 = 0.0;      // lagrangian
+ double c2 = 0.0;      // smooth vel
+ double c3 = 3.0;      // smooth coord (fujiwara)
+ double d1 = 1.0;      // surface tangent velocity u_n=u-u_t 
+ double d2 = 0.1;      // surface smooth cord (fujiwara)
+ double alpha = 1.0;   // time discrete method
+ double cfl = 0.8;
+
+ //** Physical Parameters
+ double Re = 571.732; 
+ double We = 13.6168;
+ double Fr = 31.9275;
+ double mu_in=1.2471e-05;
+ double mu_out=0.00020235;
+ double rho_in=26.016;
+ double rho_out=1156.9;
+
+ //** Solver and Pre-Conditioner Choice - pressure, velocity, scalar
+ Solver *solverP = new PetscSolver(KSPGMRES,PCILU);
+ Solver *solverV = new PetscSolver(KSPCG,PCICC);
+ Solver *solverC = new PetscSolver(KSPCG,PCICC);
+ //Solver *solverP = new PetscSolver(KSPGMRES,PCJACOBI);
+ //Solver *solverV = new PetscSolver(KSPCG,PCJACOBI);
+
+ //** Data Saving Directories
+ const char *binFolder  = "./bin/";
+ const char *mshFolder  = "./msh/";
+ const char *datFolder  = "./dat/";
+ const char *vtkFolder  = "/home/gcpoliveira/post-processing/vtk/3d/slug-pbc/";
+ 
+ //*** File
+ string meshFile = "cuboid-3d.msh";
+ 
+ //*** Peixoto's tree
+ string meshDir = (string) getenv("MESH3D_DIR");
+ meshDir += "/" + meshFile;
+ const char *mesh = meshDir.c_str();
+
+//** Constructors
+ Model3D m1;
+ Simulator3D s2;
+ Periodic3D pbc;
+
+
+ if( *(argv+1) == NULL )     
+ {
+  	cout << endl;
+  	cout << "--------------> STARTING FROM 0" << endl;
+  	cout << endl;
+
+  	const char *mesh1 = mesh;
+
+  	m1.readMSH(mesh1);
+  	m1.setInterfaceBC();
+  	m1.setTriEdge();
+  	m1.mesh2Dto3D();
+  	m1.setMapping();
+	#if NUMGLEU == 5
+ 		m1.setMiniElement();
+	#else
+ 		m1.setQuadElement();
+	#endif
+  	m1.setSurfaceConfig();
+ 	m1.setInitSurfaceVolume();
+  	m1.setInitSurfaceArea();
+  	//m1.setGenericBC();
+  	m1.setBubbleArrayPeriodicBC(); // <<<
+  	
+	pbc.MountPeriodicVectors(m1);
+  
+ 	s2(m1);
+
+  	s2.setRe(Re);
+  	s2.setWe(We);
+  	s2.setFr(Fr);
+  	s2.setC1(c1);
+  	s2.setC2(c2);
+  	s2.setC3(c3);
+  	s2.setD1(d1);
+  	s2.setD2(d2);
+  	s2.setAlpha(alpha);
+  	s2.setMu(mu_in,mu_out);
+  	s2.setRho(rho_in,rho_out);
+  	s2.setCfl(cfl);
+  	s2.init();
+  	s2.setDtALETwoPhase();
+  	s2.setSolverPressure(solverP);
+  	s2.setSolverVelocity(solverV);
+  	s2.setSolverConcentration(solverC);
+ }
+ else if( strcmp( *(argv+1),"restart") == 0 ) 
+ {
+  	cout << endl;
+  	cout << "--------------> RE-STARTING..." << endl;
+  	cout << endl;
+
+  	// load surface mesh
+  	string aux = *(argv+2);
+  	string file = (string) "./msh/newMesh-" + *(argv+2) + (string) ".msh";
+  	const char *mesh2 = file.c_str();
+  	m1.readMSH(mesh2);
+  	m1.setInterfaceBC();
+  	m1.setTriEdge();
+  	m1.mesh2Dto3D();
+
+  	s2(m1);
+
+  	// load 3D mesh
+  	file = (string) "./vtk/sim-" + *(argv+2) + (string) ".vtk";
+  	const char *vtkFile = file.c_str();
+
+  	m1.readVTK(vtkFile);
+  	m1.setMapping();
+	#if NUMGLEU == 5
+  	m1.setMiniElement();
+	#else
+  	m1.setQuadElement();
+	#endif
+  	m1.readVTKHeaviside(vtkFile);
+  	m1.setSurfaceConfig();
+  	m1.setInitSurfaceVolume();
+  	m1.setInitSurfaceArea();
+  	//m1.setGenericBC();
+  	m1.setBubbleArrayPeriodicBC(); // <<<
+  
+  	s2(m1);
+
+  	s2.setSolverPressure(solverP);
+  	s2.setSolverVelocity(solverV);
+  	s2.setSolverConcentration(solverC);
+
+  	iter = s2.loadSolution("./","sim",atoi(*(argv+2)));
+ }
+ else if( strcmp( *(argv+1),"remesh") == 0 ) 
+ {
+  	cout << endl;
+  	cout << "--------------> RE-MESHING & STARTING..." << endl;
+  	cout << endl;
+
+  	// load old mesh
+  	Model3D mOld;
+  	string file = (string) "./vtk/sim-" + *(argv+2) + (string) ".vtk";
+  	const char *vtkFile = file.c_str();
+  	mOld.readVTK(vtkFile);
+  	mOld.readVTKHeaviside(vtkFile);
+  	mOld.setMapping();
+
+  	// load surface mesh and create new mesh
+  	file = (string) "./msh/newMesh-" + *(argv+2) + (string) ".msh";
+  	const char *mesh2 = file.c_str();
+  	m1.readMSH(mesh2);
+  	m1.setInterfaceBC();
+  	m1.setTriEdge();
+  	m1.mesh2Dto3DOriginal();
+  	m1.setMapping();
+	#if NUMGLEU == 5
+  		m1.setMiniElement();
+	#else
+  	m1.setQuadElement();
+	#endif
+  	m1.setSurfaceConfig();
+  	m1.setInitSurfaceVolume();
+  	m1.setInitSurfaceArea();
+  	//m1.setGenericBC();
+  	m1.setBubbleArrayPeriodicBC(); // <<<
+
+  	s2(m1);
+
+  	s2.setSolverPressure(solverP);
+  	s2.setSolverVelocity(solverV);
+  	s2.setSolverConcentration(solverC);
+  	iter = s2.loadSolution("./","sim",atoi(*(argv+2)));
+  	s2.applyLinearInterpolation(mOld);
+ }
+ else if( strcmp( *(argv+1),"restop") == 0 )  
+ {
+  	cout << endl;
+  	cout << "--------------> RE-MESHING (NO ITERATION)..." << endl;
+  	cout << endl;
+
+  	// load old mesh
+  	Model3D mOld;
+  	string file = (string) "./vtk/sim-" + *(argv+2) + (string) ".vtk";
+  	const char *vtkFile = file.c_str();
+  	mOld.readVTK(vtkFile);
+  	mOld.readVTKHeaviside(vtkFile);
+  	mOld.setMapping();
+
+  	// load surface mesh and create new one
+  	file = (string) "./msh/newMesh-" + *(argv+2) + (string) ".msh";
+  	const char *mesh2 = file.c_str();
+  	m1.readMSH(mesh2);
+  	m1.setInterfaceBC();
+  	m1.setTriEdge();
+  	m1.mesh2Dto3DOriginal();
+  	m1.setMapping();
+	#if NUMGLEU == 5
+  	m1.setMiniElement();
+	#else
+  	m1.setQuadElement();
+	#endif
+  	m1.setSurfaceConfig();
+  	m1.setInitSurfaceVolume();
+  	m1.setInitSurfaceArea();
+
+  	s2(m1);
+  	//file = (string) "sim-" + *(argv+2);
+  	//const char *sol = file.c_str();
+  	iter = s2.loadSolution("./","sim",atoi(*(argv+2)));
+  	s2.applyLinearInterpolation(mOld);
+
+  	InOut saveEnd(m1,s2); // cria objeto de gravacao
+  	saveEnd.saveVTK(vtkFolder,"sim",atoi(*(argv+2)));
+  	saveEnd.saveMSH(mshFolder,"newMesh",atoi(*(argv+2)));
+  	saveEnd.saveSol(binFolder,"sim",atoi(*(argv+2)));
+  	//saveEnd.saveVTKSurface(vtkFolder,"sim",atoi(*(argv+2)));
+  	return 0;
+ }
+ // Point's distribution
+ Helmholtz3D h1(m1);
+ h1.setBC();
+ h1.initRisingBubble();
+ h1.assemble();
+ h1.setk(0.2);
+ h1.matMountC();
+ h1.setUnCoupledCBC(); 
+ h1.setCRHS();
+ h1.unCoupledC();
+ h1.setModel3DEdgeSize();
+
+ InOut save(m1,s2); // cria objeto de gravacao
+ save.saveVTK(vtkFolder,"geometry");
+ save.saveVTKSurface(vtkFolder,"geometry");
+ save.saveMeshInfo(datFolder);
+ save.saveInfo(datFolder,"info",mesh);
+
+ double vinst=0;
+ double vref=0;
+ int nIter = 30;
+ int nReMesh = 1;
+
+ for( int i=1;i<=nIter;i++ )
+ {
+  for( int j=0;j<nReMesh;j++ )
+  {
+
+   cout << color(none,magenta,black);
+   cout << "____________________________________ Iteration: " 
+	    << iter << endl << endl;
+   cout << resetColor();
+
+   vinst = s2.getCentroidVelXAverage();
+   vref += vinst;
+   cout << vref << " " << vinst << endl;
+   s2.setUSol(vinst);
+   m1.setGenericBC(vref);
+   s2.setURef(vref);
+
+   s2.setDtALETwoPhase();
+
+   InOut save(m1,s2); // cria objeto de gravacao
+   save.printSimulationReport();
+
+   //s2.stepLagrangian();
+   //s2.stepALE();
+   s2.stepSLPBCFix(); // <<< 
+   s2.movePoints();
+   //s2.assemble();
+   s2.assemblePBC(); // <<<
+   s2.matMount();
+   //s2.setUnCoupledBC();
+   s2.setUnCoupledPBC(); // <<<
+   //s2.setRHS();
+   s2.setRHS_PBC(); // <<<
+   //s2.setGravity("-Z");
+   //s2.setInterface();
+   s2.setInterfaceGeo();
+   //s2.unCoupled();
+   s2.unCoupledPBC(); // <<<
+
+   save.saveMSH(mshFolder,"newMesh",iter);
+   save.saveVTK(vtkFolder,"sim",iter);
+   save.saveVTKSurface(vtkFolder,"sim",iter);
+   save.saveSol(binFolder,"sim",iter);
+   save.saveBubbleInfo(datFolder);
+   //save.crossSectionalVoidFraction(datFolder,"voidFraction",iter);
+
+   s2.saveOldData();
+
+   s2.timeStep();
+
+   cout << color(none,magenta,black);
+   cout << "________________________________________ END of " 
+	    << iter << endl << endl;;
+   cout << resetColor();
+
+   iter++;
+  }
+  Helmholtz3D h2(m1,h1);
+  h2.setBC();
+  h2.initRisingBubble();
+  h2.assemble();
+  h2.setk(0.2);
+  h2.matMountC();
+  h2.setUnCoupledCBC(); 
+  h2.setCRHS();
+  h2.unCoupledC();
+  h2.saveChordalEdge(datFolder,"edge",iter-1);
+  h2.setModel3DEdgeSize();
+
+  Model3D mOld = m1; 
+
+  /* *********** MESH TREATMENT ************* */
+  // set normal and kappa values
+  m1.setNormalAndKappa();
+  m1.initMeshParameters();
+
+  // 3D operations
+  //m1.insert3dMeshPointsByDiffusion();
+  m1.remove3dMeshPointsByDiffusion();
+  //m1.removePointByVolume();
+  //m1.removePointsByInterfaceDistance();
+  //m1.remove3dMeshPointsByDistance();
+  m1.remove3dMeshPointsByHeight();
+  m1.delete3DPoints();
+
+  // surface operations
+  m1.smoothPointsByCurvature();
+
+  m1.insertPointsByLength("flat");
+  //m1.insertPointsByCurvature("flat");
+  //m1.removePointsByCurvature();
+  //m1.insertPointsByInterfaceDistance("flat");
+  m1.contractEdgesByLength("flat");
+  //m1.removePointsByLength();
+  m1.flipTriangleEdges();
+
+  m1.removePointsByNeighbourCheck();
+  //m1.checkAngleBetweenPlanes();
+  /* **************************************** */
+
+  //m1.mesh2Dto3DOriginal();
+  m1.mesh3DPoints();
+  m1.setMapping();
+#if NUMGLEU == 5
+ m1.setMiniElement();
+#else
+ m1.setQuadElement();
+#endif
+  m1.setSurfaceConfig();
+  m1.setInterfaceBC();
+  m1.setGenericBC(vref);
+
+  Simulator3D s3(m1,s2);
+  s3.applyLinearInterpolation(mOld);
+  s2 = s3;
+  s2.setSolverPressure(solverP);
+  s2.setSolverVelocity(solverV);
+  s2.setSolverConcentration(solverC);
+
+  InOut saveEnd(m1,s2); // cria objeto de gravacao
+  saveEnd.printMeshReport();
+  saveEnd.saveMeshInfo(datFolder);
+ }
+
+ PetscFinalize();
+ return 0;
+}
+
+
